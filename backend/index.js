2849cb42db098b0c6e47a68fd4827aeb3e96ef03
jitu7033 <jraj41797@gmail.com>
2025-08-11 11:16:14 +0530

    function base to integer

--- backend/index.js
+++ backend/index.js
@@ -12,3 +12,23 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+
+function parseBaseToInteger(str,base){
+  const b = BigInt(base);
+  let s = str.trim().toLowerCase();
+
+  let val = 0n;
+
+  for(let ch of s){
+    let digit;
+     if (ch >= '0' && ch <= '9') digit = BigInt(ch.charCodeAt(0) - '0'.charCodeAt(0));
+     else if (ch >= 'a' && ch <= 'z') digit = BigInt(10 + ch.charCodeAt(0) - 'a'.charCodeAt(0));
+     else {
+      throw new Error(`Invalid character '${ch}' in number ${str}`);
+    }
+    if (digit >= b) throw new Error(`Digit ${digit} out of range for base ${base} in ${str}`);
+    value = value * b + digit;
+  }
+  return value;
+}
